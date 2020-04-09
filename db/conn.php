e9b87d2e23401356a4702cebe7442d014c26d0e0
Markus <ModernFuzzBall2@hotmail.com>
2020-04-09 16:20:05 -0400

    Initial commit

---
+++ db/conn.php
@@ -0,0 +1,22 @@
+<?php 
+    $host = 'localhost'; // 127.0.0.1 = localhost
+    $port= '8080';
+    $db = 'attendance_db';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        throw new PDOException($e->getMessage());
+    }
+
+    require_once 'crud.php';
+    $crud = new crud($pdo);
+
+?>
