25e97d95043b4e9c48a2547a7b0b020f7f0c4cea
Markus <modernfuzzball2@hotmail.com>
2021-04-07 23:18:45 -0400

    Apparently I never uploaded this

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,19 @@
 <?php 
+    // Development Connection
     $host = 'localhost'; // 127.0.0.1 = localhost
     $port= '8080';
     $db = 'attendance_db';
     $user = 'root';
     $pass = '';
     $charset = 'utf8mb4';
+
+    // Live Database Connection
+    // $host = 'localhost'; // 127.0.0.1 = localhost
+    // $port= '8080';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
@@ -17,6 +26,9 @@
     }
 
     require_once 'crud.php';
+    require_once 'user.php';
     $crud = new crud($pdo);
+    $user = new user($pdo);
 
+    $user->insertUser("admin","password");
 ?>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
+<?php
+//This includes the session file. This file contains code that starts/resumes a session.
+//By having it in the header file, it will be included on every page, allowing session capability on every page across the website.
+include_once 'includes/session.php'?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -26,13 +31,23 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="viewrecords.php">View Attendees</a>
                         </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        
+                            <?php if(!isset($_SESSION['userid'])) { ?>
+                                <li class="nav-item"><a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a></li>
+                            <?php } else { ?>
+                                <li class="nav-item"><a class="nav-link" href="#"><span>Hello <?php echo $_SESSION['username'] ?>! </span> <span class="sr-only">(current)</span></a></li>
+                                <li class="nav-item"><a class="nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a></li>
+                            <?php } ?>
+
                     </ul>
                 </div>
             </nav>
